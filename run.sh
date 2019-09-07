b8ab0b6efda372b6e3be8f6c5d32a013edb96cf0
Tim-M. Decker <tide8011@colorado.edu>
2019-09-07 16:44:11 -0600

    clean and fix bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#!/bin/sh
+
 PATH=/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+eval "$(conda shell.bash hook)"  # fixes potential CommandNotFoundError with conda activate
+conda activate swe4s
+echo this functions runs math_lib.py
+echo ... running ...
 python calculate.py
